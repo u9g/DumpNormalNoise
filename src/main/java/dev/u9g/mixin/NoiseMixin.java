c9ee4f4e3eb470d61e4c60463ccd2d948ea9f5e6
u9g <43508353+u9g@users.noreply.github.com>
2022-05-24 22:06:32 -0400

    change to mojmaps and print all vars

--- src/main/java/dev/u9g/mixin/NoiseMixin.java
+++ src/main/java/dev/u9g/mixin/NoiseMixin.java
@@ -3,42 +3,58 @@ package dev.u9g.mixin;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import dev.u9g.DumpNormalNoise;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.world.gen.noise.NoiseParametersKeys;
-import net.minecraft.world.gen.random.RandomDeriver;
-import net.minecraft.world.gen.random.Xoroshiro128PlusPlusRandom;
-import net.minecraft.world.gen.random.Xoroshiro128PlusPlusRandomImpl;
+import net.minecraft.world.level.levelgen.*;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
-@Mixin(NoiseParametersKeys.class)
+@Mixin(XoroshiroRandomSource.XoroshiroPositionalRandomFactory.class)
 public class NoiseMixin {
-    @Inject(at = @At("HEAD"), method = "method_41127(Lnet/minecraft/world/gen/random/RandomDeriver;Lnet/minecraft/util/registry/RegistryEntry;)Lnet/minecraft/util/math/noise/DoublePerlinNoiseSampler;")
-    private static void method_41127(RandomDeriver randomDeriver, RegistryEntry<DoublePerlinNoiseSampler.NoiseParameters> registryEntry, CallbackInfoReturnable<DoublePerlinNoiseSampler> cir) {
-        Identifier location = registryEntry.getKey().orElseThrow().getValue();
-        if (randomDeriver instanceof Xoroshiro128PlusPlusRandom.RandomDeriver rd) {
-            Xoroshiro128PlusPlusRandomImpl impl = (((XoroRandomAccessor) rd.createRandom(location)).implementation());
-            long seedLo = ((XoroRandomImplMixin)impl).seedLo();
-            long seedHi = ((XoroRandomImplMixin)impl).seedHi();
+    @Shadow @Final private long seedLo;
+
+    @Shadow @Final private long seedHi;
+
+    @Inject(at = @At("TAIL"), method = "fromHashOf(Ljava/lang/String;)Lnet/minecraft/world/level/levelgen/RandomSource;", locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    public void fromHashOf(String string, CallbackInfoReturnable<RandomSource> cir, byte[] bs, long l, long m) {
             JsonObject obj = new JsonObject();
-            obj.addProperty("name", location.toString());
-            obj.addProperty("seedLo", ((Long)seedLo).toString());
-            obj.addProperty("seedHi", ((Long)seedHi).toString());
+            obj.addProperty("name", string);
+            obj.addProperty("seedLo", ((Long)this.seedLo).toString());
+            obj.addProperty("seedHi", ((Long)this.seedHi).toString());
+            obj.addProperty("l", ((Long)l).toString());
+            obj.addProperty("m", ((Long)m).toString());
             StackTraceElement[] elems = new Throwable().getStackTrace();
             JsonArray stacktrace = new JsonArray();
-            for (String elem : Arrays.stream(elems).map(e -> e.getClassName()+"#"+e.getMethodName()+":"+e.getLineNumber()).toList()) {
+            for (String elem : Arrays.stream(elems).map(e -> e.getClassName()+"#"+e.getMethodName()+":"+e.getLineNumber()).limit(10).toList()) {
                 stacktrace.add(elem);
             }
             obj.add("stacktrace", stacktrace);
             DumpNormalNoise.XORO.add(obj);
-//            System.out.println(location+" {\n  seedlo: "+seedLo+"\n  seedhi: "+seedHi+"\n}");
-        }
     }
+//    @Inject(at = @At("HEAD"), method = "instantiate(Lnet/minecraft/world/level/levelgen/PositionalRandomFactory;Lnet/minecraft/core/Holder;)Lnet/minecraft/world/level/levelgen/synth/NormalNoise;")
+//    private static NormalNoise instantiate(PositionalRandomFactory positionalRandomFactory, Holder<NormalNoise.NoiseParameters> holder, CallbackInfoReturnable<NormalNoise> cir) {
+//        ResourceLocation location = holder.unwrapKey().orElseThrow().location();
+//        if (positionalRandomFactory instanceof XoroshiroRandomSource.XoroshiroPositionalRandomFactory rd) {
+//            Xoroshiro128PlusPlus impl = (((XoroRandomAccessor) rd.fromHashOf(location)).randomNumberGenerator());
+//            long seedLo = ((XoroRandomImplMixin)impl).seedLo();
+//            long seedHi = ((XoroRandomImplMixin)impl).seedHi();
+//            JsonObject obj = new JsonObject();
+//            obj.addProperty("name", location.toString());
+//            obj.addProperty("seedLo", ((Long)seedLo).toString());
+//            obj.addProperty("seedHi", ((Long)seedHi).toString());
+//            StackTraceElement[] elems = new Throwable().getStackTrace();
+//            JsonArray stacktrace = new JsonArray();
+//            for (String elem : Arrays.stream(elems).map(e -> e.getClassName()+"#"+e.getMethodName()+":"+e.getLineNumber()).toList()) {
+//                stacktrace.add(elem);
+//            }
+//            obj.add("stacktrace", stacktrace);
+//            DumpNormalNoise.XORO.add(obj);
+////            System.out.println(location+" {\n  seedlo: "+seedLo+"\n  seedhi: "+seedHi+"\n}");
+//        }
+//    }
 }
--- src/main/java/dev/u9g/mixin/ReadyMixin.java
+++ src/main/java/dev/u9g/mixin/ReadyMixin.java
@@ -2,15 +2,8 @@ package dev.u9g.mixin;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import dev.u9g.DumpNormalNoise;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.dedicated.MinecraftDedicatedServer;
-import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.server.dedicated.DedicatedServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,27 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.io.FileWriter;
 import java.io.Writer;
 
-@Mixin(MinecraftDedicatedServer.class)
+@Mixin(DedicatedServer.class)
 public class ReadyMixin {
-    @Inject(at = @At("TAIL"), method = "setupServer()Z")
+    @Inject(at = @At("TAIL"), method = "initServer()Z")
     public void init(CallbackInfoReturnable<Boolean> cir) {
         try (Writer writer = new FileWriter("output.json")) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//                JsonObject obj = new JsonObject();
-//                obj.addProperty("seed", ((MinecraftServer)FabricLoader.getInstance().getGameInstance()).getOverworld().getSeed());
-//                JsonArray noise = new JsonArray();
-//                for (DoublePerlinNoiseSampler.NoiseParameters param : BuiltinRegistries.NOISE_PARAMETERS) {
-//                    JsonObject noiseObj = new JsonObject();
-//                    noiseObj.addProperty("firstOctave", param.firstOctave());
-//                    JsonArray amplitude = new JsonArray();
-//                    for (Double amp : param.amplitudes()) {
-//                        amplitude.add(amp);
-//                    }
-//                    noiseObj.add("amplitude", amplitude);
-//                    noise.add(noiseObj);
-//                }
-//                obj.add("noise", noise);
-//                gson.toJson(obj, writer);
             gson.toJson(DumpNormalNoise.XORO, writer);
         } catch (Exception e) {
             e.printStackTrace();
